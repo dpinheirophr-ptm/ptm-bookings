68f3c6f62e570a8edac86da7a04f1741edeefb00
dpinheirophr-ptm <dpinheiro.phr@gmail.com>
2026-05-31 00:07:56 +1000

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDURkS-FTU7ULyEujCL4IRGA0-pzn41zbs",
+  authDomain: "ptm--bookings.firebaseapp.com",
+  projectId: "ptm--bookings",
+  storageBucket: "ptm--bookings.firebasestorage.app",
+  messagingSenderId: "526233519147",
+  appId: "1:526233519147:web:fb5efb4b79a12f1408966e"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
